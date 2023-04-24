f9da7066c73e7aec55842fb2bce4d01f652a609a
Maciej Zalega <130605144+Riveas@users.noreply.github.com>
2023-04-24 15:39:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,42 +18,38 @@ test = apl.loc['2020-1-2':].copy()
 scaler = MinMaxScaler(feature_range = (0, 1))
 aplScaled = scaler.fit_transform(train['Close'].values.reshape(-1, 1))
 
-tail = 30
+back = 30
 
 xTrain = []
 yTrain = []
 
-for x in range (tail, len(aplScaled)):
-    xTrain.append(aplScaled[x-tail:x, 0])
+for x in range (back, len(aplScaled)):
+    xTrain.append(aplScaled[x - back:x, 0])
     yTrain.append(aplScaled[x, 0])
 
 xTrain, yTrain = np.array(xTrain), np.array(yTrain)
 xTrain = np.reshape(xTrain, (xTrain.shape[0], xTrain.shape[1], 1))
 
 model = Sequential()
-model.add(LSTM(units = 50, return_sequences = True, input_shape = (tail, 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units = 50, return_sequences = True))
-model.add(Dropout(0.2))
+model.add(LSTM(units = 200, return_sequences = True, input_shape = (back, 1)))
 model.add(LSTM(units = 50))
-model.add(Dropout(0.2))
 model.add(Dense(units = 1))
 
 model.compile(optimizer = 'adam', loss = 'mse')
-model.fit(xTrain, yTrain, epochs = 50, batch_size = 25)
+model.fit(xTrain, yTrain, epochs = 25, batch_size = 25)
 
 print(test['Close'].values)
 
 total = pd.concat((train['Close'], test['Close']), axis = 0)
 
-modelInput = total[len(total)-len(test)-tail:].values
+modelInput = total[len(total) - len(test) - back:].values
 modelInput = modelInput.reshape(-1, 1)
 modelInputScaled = scaler.transform(modelInput)
 
 xTest = []
 
-for x in range (tail, len(modelInputScaled)):
-    xTest.append(modelInputScaled[x-tail:x, 0])
+for x in range (back, len(modelInputScaled)):
+    xTest.append(modelInputScaled[x - back:x, 0])
 
 xTest = np.array(xTest)
 xTest = np.reshape(xTest, (xTest.shape[0], xTest.shape[1], 1))
